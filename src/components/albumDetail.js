d3230d60680b9cc9ebdac251aae861aff5871830
Davideuijinlee <Davideuijinlee@gmail.com>
2019-07-24 16:40:53 -0700

    added additional view tags

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -7,8 +7,19 @@ export default props => {
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View></View>
+                <View stle={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
             </CardSection>
         </Card>
     )
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 };
